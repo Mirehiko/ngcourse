a614c92fb6d110ffbdd99690e3184b7ad7b9396b
mirehiko <mirehiko@gmail.com>
2023-08-15 10:26:32 +0500

    DI-31

--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -10,7 +10,9 @@ import { RandomService } from '../random.service';
     RandomLazyComponent,
     LazyAppComponent,
   ],
-  providers:[{provide: RandomService3, useClass: RandomService}],
+  // providers:[{provide: RandomService3, useClass: RandomService}],// 29
+  providers:[{provide: RandomService3, useExisting: RandomService}],// 31
+
   imports: [
       RouterModule.forChild([
       { path: '', component: LazyAppComponent }
