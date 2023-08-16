455d6964a3c4845da71b59d48f69f38855c432a4
mirehiko <mirehiko@gmail.com>
2023-08-16 19:29:37 +0500

    f

--- src/app/directives/generate-input.directive.ts
+++ src/app/directives/generate-input.directive.ts
@@ -1,34 +1,57 @@
-import {Component, Directive, ElementRef, Input, Renderer2, TemplateRef, ViewContainerRef} from "@angular/core";
+import {
+    Component,
+    Directive,
+    ElementRef,
+    Input,
+    OnChanges,
+    Renderer2,
+    SimpleChanges,
+    TemplateRef,
+    ViewContainerRef
+} from "@angular/core";
 
 
 @Component({
     selector: 'm-email',
     template: 'email'
 })
-export class MEmailComponent {}
+export class MEmailComponent {
+}
 
 @Component({
     selector: 'm-number',
     template: 'number'
 })
-export class MNumberComponent {}
+export class MNumberComponent {
+}
 
 @Directive({
     selector: '[inputCount]',
     standalone: true
 })
-export class GenerateCustomInputDirective {
+export class GenerateCustomInputDirective implements OnChanges {
     private _initial: string[] = [];
-    @Input() public inputCountInitial: string[] = [];
+
     @Input('inputCountInitial') set initial(vals: string[]) {
         console.log(vals);
+        this._initial = vals;
+    }
+    get initial(): string[] {
+        return this._initial;
     }
 
-    constructor(private templateRef: TemplateRef<any>, private viewRef: ViewContainerRef) {}
+
+    constructor(private templateRef: TemplateRef<any>, private viewRef: ViewContainerRef) {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if ('initial' in changes) {
+            console.log(changes)
+        }
+    }
 
     @Input() set inputCount(nums: string[]) {
-        console.log(this.inputCountInitial)
-        nums.forEach((current, index) => {
+        this.initial.forEach((current, index) => {
             if (current === 'email') {
                 this.viewRef.createComponent(MEmailComponent)
             }
